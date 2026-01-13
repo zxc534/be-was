375c0301e14dbc2ea3ac872ee1e89726c081ad50
zxc534 <zxc534@naver.com>
2026-01-13 16:53:23 +0900

    [feat] 아티클 페이지에 이미지 경로를 포함하도록 수정

--- src/main/java/webserver/ActionMap.java
+++ src/main/java/webserver/ActionMap.java
@@ -291,6 +291,7 @@ public class ActionMap {
             Map<String, String> variables = new HashMap<>();
             variables.put("userId", article.getUserId());
             variables.put("content", article.getContent());
+            variables.put("imgFileName", article.getImgFileName());
 
             Response rsp = new Response();
             rsp.resultCode = ResultCode.OK;
